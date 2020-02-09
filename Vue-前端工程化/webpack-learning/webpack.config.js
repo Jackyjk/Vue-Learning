f903c8304844bd7c0bc981725cfcbab37ab86e0f
Jackyjk <2538808265@qq.com>
2020-02-09 18:02:22 +0800

    所有插件 部署完毕  上传

--- Vue-前端工程化/webpack-learning/webpack.config.js
+++ Vue-前端工程化/webpack-learning/webpack.config.js
@@ -1,33 +1,28 @@
-const path = require("path");
-
-//导入包
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-//创建对象
-const htmlPlugin = new HtmlWebpackPlugin({
-    //设置生成预览页面的模板文件
-    template: "./src/index.html",
-    //设置生成的预览页面名称
-    filename: "index.html"
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const htmlPlguin = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html'
 })
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-    mode: "development", //可以设置为development(开发模式)，production(发布模式)
-    //设置入口文件路径
-    entry: path.join(__dirname, "./src/index.js"),
-    //设置出口文件
-    output: {
-        //设置路径
-        path: path.join(__dirname, "./dist"),
-        //设置文件名
-        filename: "bundle.js"
-    },
-    plugins: [htmlPlugin],
-    module: {
-        rules: [{
-            //test设置需要匹配的文件类型，支持正则
-            test: /\.css$/,
-            //use表示该文件类型需要调用的loader
-            use: ['style-loader', 'css-loader']
-        }]
-    }
+  // 编译模式
+  mode: 'development', // development  production
+  entry: path.join(__dirname, './src/index.js'),
+  output: {
+    path: path.join(__dirname, './dist'), // 输出文件的存放路径
+    filename: 'bundle.js' // 输出文件的名称
+  },
+  plugins: [htmlPlguin, new VueLoaderPlugin()],
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+      //{ test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /\.jpg|png|gif|bmp|ttf|eot|svg|woff|woff2$/, use: 'url-loader?limit=16941' },
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.vue$/, use: 'vue-loader' }
+    ]
+  }
 }
