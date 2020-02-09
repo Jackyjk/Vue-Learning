b8e2f633e9485b2136ae818b9998c72113bc1ed2
Jackyjk <2538808265@qq.com>
2020-02-09 16:08:55 +0800

    html-webpack-plugin  直接打开预览页面

--- Vue-前端工程化/webpack-learning/webpack.config.js
+++ Vue-前端工程化/webpack-learning/webpack.config.js
@@ -1,13 +1,25 @@
 const path = require("path");
-    module.exports = {
-        mode:"development",//可以设置为development(开发模式)，production(发布模式)
-        //设置入口文件路径
-        entry: path.join(__dirname,"./src/index.js"),
-        //设置出口文件
-        output:{
-            //设置路径
-            path:path.join(__dirname,"./dist"),
-            //设置文件名
-            filename:"bundle.js"
-        }
-    }
+
+//导入包
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+//创建对象
+const htmlPlugin = new HtmlWebpackPlugin({
+    //设置生成预览页面的模板文件
+    template: "./src/index.html",
+    //设置生成的预览页面名称
+    filename: "index.html"
+})
+
+module.exports = {
+    mode: "development", //可以设置为development(开发模式)，production(发布模式)
+    //设置入口文件路径
+    entry: path.join(__dirname, "./src/index.js"),
+    //设置出口文件
+    output: {
+        //设置路径
+        path: path.join(__dirname, "./dist"),
+        //设置文件名
+        filename: "bundle.js"
+    },
+    plugins: [htmlPlugin]
+}
